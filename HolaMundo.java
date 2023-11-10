e346ae70785e1012233b41b4ce10709139f6bbf6
Antonio Leon <antleonfdez01@gmail.com>
2023-11-10 10:52:43 +0100

    Version inicial del fichero HolaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+/**
+ * muestra por pantalla la frase Hola Mundo!
+ */
+public class HolaMundo{
+    public static void main (String[] args){
+        System.out.println("Hola Mundo!");
+        System.out.println("Aqui estamos haciendo pruebas en Git");
+    }
+}
